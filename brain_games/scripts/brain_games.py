f3f7d67bc9d6abf2c6853bb2fb06db847d26749e
Niko <eleron96@gmail.com>
2022-07-07 14:50:47 +0300

    New function of Welcome

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,5 +1,11 @@
+#!/usr/bin/env python
+
+import prompt
+
 def main():
     print("Welcome to the Brain Games!")
+    name = prompt.string('May I have your name? ')
+    print(f"Hello, {name}!")
 
 if __name__ == '__main__':
     main()
