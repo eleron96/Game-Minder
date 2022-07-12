390fd38c6bc68b74c11a8173ca1cda697af1af41
Niko <eleron96@gmail.com>
2022-07-12 19:47:12 +0300

    Brain game even or odd

---
+++ brain_games/scripts/brain_even.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python -tt
+from random import randint
+
+
+
+def brain_even():
+    i = 0
+    
+    print('Answer "yes" if the number is even, otherwise answer "no".')
+    while i <= 3:
+        for _ in range(10):
+	        value = randint(0, 10)
+
+        print("Question: "+str(value))
+        useranswer = input('Your answer: ')  
+
+        if (value % 2) == 0:
+            if useranswer == "yes":
+                print("Correct!")
+                i+=1
+            else:
+                print(f"Let's try again,!")
+        else:
+            if useranswer == "no":
+                print("Correct!")
+                i+=1
+            else:                
+                print(f"Let's try again,!")
+
+brain_even()
