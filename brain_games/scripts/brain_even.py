d62489db37200dfaca171750b90d2de2eba956f9
Niko <eleron96@gmail.com>
2022-07-16 19:32:12 +0300

    reformated code of brain even

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -2,8 +2,8 @@
 from random import randint
 import prompt
 
-def brain_even():
 
+def brain_even():
     print("Welcome to the Brain Games!")
     name = prompt.string('May I have your name? ')
     print(f"Hello, {name}!")
@@ -12,28 +12,29 @@ def brain_even():
     print('Answer "yes" if the number is even, otherwise answer "no".')
     while i <= 3:
         for _ in range(10):
-	        value = randint(0, 10)
+            value = randint(0, 10)
 
-        print("Question: "+str(value))
-        useranswer = input('Your answer: ')  
+        print("Question: " + str(value))
+        useranswer = input('Your answer: ')
 
         if (value % 2) == 0:
             if useranswer == "yes":
                 print("Correct!")
-                i+=1
+                i += 1
             else:
                 print(f"Let's try again,{name}!")
         else:
             if useranswer == "no":
                 print("Correct!")
-                i+=1
-            else:                
+                i += 1
+            else:
                 print(f"Let's try again,{name}!")
     print(f"Congratulations, {name}!")
 
-if __name__ == '__main__':
-    main()
 
 def main():
     brain_even()
 
+
+if __name__ == '__main__':
+    main()
