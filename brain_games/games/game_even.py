67d105046eb9d568afcaad1f33e8e48fd28a230c
Niko <eleron96@gmail.com>
2022-09-20 14:33:59 +0300

    рефакторинг

--- brain_games/games/game_even.py
+++ brain_games/games/game_even.py
@@ -1,32 +1,43 @@
 #!/usr/bin/python -tt
 
 from random import randint
-from brain_games.engine import welcome_user, user_lose
 
-name = welcome_user("brain_even")
+RULES = 'Answer "yes" if the number is even, otherwise answer "no".'
 
 
 def brain_even():
-    i = 1
-    while i <= 3:
-        for _ in range(10):
-            value = randint(0, 10)
+    for _ in range(10):
+        value = randint(0, 10)
+    return value
 
-        print("Question: " + str(value))
-        useranswer = input('Your answer: ')
 
-        if (value % 2) == 0:
-            if useranswer == "yes":
-                print("Correct!")
-                i += 1
-            else:
-                answer = "yes"
-                user_lose(useranswer, answer, name)
-        else:
-            if useranswer == "no":
-                print("Correct!")
-                i += 1
-            else:
-                answer = "no"
-                user_lose(useranswer, answer, name)
-    print(f"Congratulations, {name}!")
+def is_even(number):
+    if (number % 2) == 0:
+        return "yes"
+    else:
+        return "no"
+
+# def brain_even():
+#     i = 1
+#     while i <= 3:
+#         for _ in range(10):
+#             value = randint(0, 10)
+#
+#         print("Question: " + str(value))
+#         useranswer = input('Your answer: ')
+#
+#         if (value % 2) == 0:
+#             if useranswer == "yes":
+#                 print("Correct!")
+#                 i += 1
+#             else:
+#                 answer = "yes"
+#                 user_lose(useranswer, answer, name)
+#         else:
+#             if useranswer == "no":
+#                 print("Correct!")
+#                 i += 1
+#             else:
+#                 answer = "no"
+#                 user_lose(useranswer, answer, name)
+#     print(f"Congratulations, {name}!")
--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,10 +1,17 @@
 #!/usr/bin/python -tt
 
-from ..games.game_even import brain_even
+
+from brain_games.engine import welcome_user, ask_name, welcome_user_by_name, round_start, user_answer, \
+    user_lose, game_rules
+from brain_games.games import game_even
 
 
 def main():
-    brain_even()
+    welcome_user()
+    name = ask_name()
+    welcome_user_by_name(name)
+    # game_rules("brain_even")
+    round_start(game_even)
 
 
 if __name__ == '__main__':
