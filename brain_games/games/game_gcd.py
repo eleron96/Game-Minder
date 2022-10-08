bb0b281bddc88a9d5ba6c649501cd334016c0a6d
niko gamsakhurdiya <eleron96@gmail.com>
2022-10-08 19:27:52 +0300

    changed type of answer in engine 7

--- brain_games/games/game_gcd.py
+++ brain_games/games/game_gcd.py
@@ -8,7 +8,7 @@ def generate_round():
     random_number_one = randint(0, 1000)
     random_number_two = randint(0, 1000)
 
-    random_number = f'{random_number_one} and {random_number_two} '
+    random_number = f'{random_number_one} {random_number_two} '
 
     while random_number_one != 0 and random_number_two != 0:
         if random_number_one > random_number_two:
