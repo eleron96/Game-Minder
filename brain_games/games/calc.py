bba77b139860b233d762d215e9f5cf867f74e936
niko gamsakhurdiya <eleron96@gmail.com>
2022-10-18 23:53:45 +0300

    changed else\for and prime function 3

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -8,15 +8,17 @@ def generate_round():
     random_number_one = randint(1, 100)
     random_number_two = randint(1, 100)
     expression = ["+", "-", "*"]
+    operator = random.choice(expression)
 
-    if random.choice(expression) == '+':
+    if operator == '+':
         correct_answer = random_number_one + random_number_two
         question_expression = f"{random_number_one} + {random_number_two}"
-    elif random.choice(expression) == '-':
+    elif operator == '-':
         correct_answer = random_number_one - random_number_two
         question_expression = f"{random_number_one} - {random_number_two}"
-    else:
+    elif operator == '*':
         correct_answer = random_number_one * random_number_two
         question_expression = f"{random_number_one} * {random_number_two}"
 
     return question_expression, str(correct_answer)
+
--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -14,8 +14,8 @@ def is_prime(n):
 
     if n == 0 or n == 1:
         return False
-    else:
-        for i in range(2, int(sqrt(n)) + 1):
-            if (n % i) == 0:
-                return False
-        return True
+
+    for i in range(2, int(sqrt(n)) + 1):
+        if (n % i) == 0:
+            return False
+    return True
